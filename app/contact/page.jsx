642d1154809dc8af7dc9b19d02f4702f4963b787
en-wai <nyasare@hotmail.com>
2025-06-04 20:40:46 +0000

    Feat: Personalise the landing page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form className="flex flex-col gap-6 p-10 bg-[#000000]rounded-xl"> //#27272c
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
                 Have an idea, project, or challenge you'd like to collaborate on? 
--- app/page.jsx
+++ app/page.jsx
@@ -13,12 +13,12 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-xl">Army Officer and Tech Enthusiast</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Luke Coleman</span>
+              welcome to <br /> <span className="text-accent">DextersDiary</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experiences and I am
+              I excel at crafting elegant tech-first experiences and solutions and I am
               proficient in various programming languages and technologies.
             </p>
             {/* btn and socials */}
